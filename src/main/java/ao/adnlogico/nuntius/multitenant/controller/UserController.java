ede3e8f1cec9bcff939218d6b2c7d407f6a3f2c4
Domingos Masta <e1000014692@gmail.com>
2021-01-29 16:55:14 +0100

    Crud User

--- src/main/java/ao/adnlogico/nuntius/multitenant/controller/UserController.java
+++ src/main/java/ao/adnlogico/nuntius/multitenant/controller/UserController.java
@@ -6,17 +6,29 @@
 package ao.adnlogico.nuntius.multitenant.controller;
 
 import ao.adnlogico.nuntius.multitenant.dto.AuthResponse;
+import ao.adnlogico.nuntius.multitenant.exception.EntityNotFoundException;
 import ao.adnlogico.nuntius.multitenant.security.RequestAuthorization;
 import ao.adnlogico.nuntius.multitenant.tenant.entity.User;
 import ao.adnlogico.nuntius.multitenant.tenant.model_assembler.UserModelAssembler;
 import ao.adnlogico.nuntius.multitenant.tenant.repository.UserRepository;
 import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author domingos.fernando
  */
 @RestController
-@RequestMapping("/nuntius/v1/api/user")
+@RequestMapping("/nuntius/v1/api")
 public class UserController implements Serializable
 {
 
@@ -38,9 +50,6 @@ public class UserController implements Serializable
     private final UserRepository repository;
     private final UserModelAssembler assembler;
 
-    @Autowired
-    ApplicationContext applicationContext;
-
     public UserController(UserRepository repository, UserModelAssembler assembler)
     {
 
@@ -49,12 +58,107 @@ public class UserController implements Serializable
     }
 
     @RequestAuthorization
-    @RequestMapping(value = "/current", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/current", method = RequestMethod.GET)
     public ResponseEntity<?> userInfo(@RequestParam String userName) throws AuthenticationException
     {
         User current = repository.findByEmail(userName);
         AuthResponse response = new AuthResponse(current.getId(), current.getEmail(), "");
         return ResponseEntity.ok(response);
+    }
+
+    @RequestAuthorization
+    @GetMapping("/user")
+    public CollectionModel<EntityModel<User>> all()
+    {
+        List<EntityModel<User>> users = repository.findAll().stream() //
+                .map(assembler::toModel) //
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
+    }
+
+    @RequestAuthorization
+    @PostMapping("/user")
+    public ResponseEntity<?> save(@RequestBody User user)
+    {
+        EntityModel<User> entityModel = assembler.toModel(repository.save(user));
+
+        return ResponseEntity //
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
+    }
+
+    @RequestAuthorization
+    @GetMapping("/user/{id}")
+    public EntityModel<User> findById(@PathVariable Long id)
+    {
+        User user = repository.findById(id) //
+                .orElseThrow(() -> new EntityNotFoundException(new User(), id));
+
+        return assembler.toModel(user);
+    }
+
+    @RequestAuthorization
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> update(@RequestBody User newUser, @PathVariable Long id)
+    {
+        User updatedUser = repository.findById(id) //
+                .map(user -> {
+                    user.setPhone(newUser.getPhone());
+                    user.setPhoneAlt(newUser.getPhoneAlt());
+                    user.setMechanographicNumber(newUser.getMechanographicNumber());
+                    user.setDescription(newUser.getDescription());
+                    user.setUpdatedAt(newUser.getCreatedAt());
+                    user.setFkFunction(newUser.getFkFunction());
+                    user.setFkRole(newUser.getFkRole());
+                    return repository.save(user);
+                }) //
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+
+        EntityModel<User> entityModel = assembler.toModel(updatedUser);
+
+        return ResponseEntity //
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
+    }
+
+    @RequestAuthorization
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updatePassword(@RequestBody User newUser, @PathVariable Long id)
+    {
+        User updatedUser = repository.findById(id) //
+                .map(user -> {
+                    user.setPhone(newUser.getPhone());
+                    user.setPhoneAlt(newUser.getPhoneAlt());
+                    user.setMechanographicNumber(newUser.getMechanographicNumber());
+                    user.setDescription(newUser.getDescription());
+                    user.setUpdatedAt(newUser.getCreatedAt());
+                    user.setFkFunction(newUser.getFkFunction());
+                    user.setFkRole(newUser.getFkRole());
+                    return repository.save(user);
+                }) //
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+
+        EntityModel<User> entityModel = assembler.toModel(updatedUser);
+
+        return ResponseEntity //
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
+    }
+
+    @RequestAuthorization
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id)
+    {
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
